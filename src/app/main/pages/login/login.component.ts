cffef1581b2aac53b9fcac0d2ee2b567c2f2f3a2
Nestor Martinez <nestor-mh@outlook.com>
2019-10-09 13:08:42 -0500

    login from

--- src/app/main/pages/login/login.component.ts
+++ src/app/main/pages/login/login.component.ts
@@ -5,11 +5,23 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
+
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public year_now: string = new Date().getFullYear().toString();
+  public visibility_icon: boolean;
+  public password_type: string;
+
+  constructor() { 
+    this.showVisibilitPassword();
+  }
 
   ngOnInit() {
+  }
+
+  showVisibilitPassword(){
+    this.password_type = (this.visibility_icon) ? "text":"password";
+    this.visibility_icon = !this.visibility_icon    
   }
 
 }
